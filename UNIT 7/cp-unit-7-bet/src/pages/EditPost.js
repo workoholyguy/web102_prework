e4d86893f76e35e78f716720a968078f3863fe61
omarmadjitov <orules00@icloud.com>
2024-10-29 21:05:10 -0400

    Initial commit

---
+++ UNIT 7/cp-unit-7-bet/src/pages/EditPost.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import './EditPost.css'
+
+const EditPost = ({data}) => {
+
+    const {id} = useParams();
+    const [post, setPost] = useState({id: null, title: "", author: "", description: ""});
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setPost( (prev) => {
+            return {
+                ...prev,
+                [name]:value,
+            }
+        })
+    }
+
+    return (
+        <div>
+            <form>
+                <label for="title">Title</label> <br />
+                <input type="text" id="title" name="title" value={post.title} onChange={handleChange} /><br />
+                <br/>
+
+                <label for="author">Author</label><br />
+                <input type="text" id="author" name="author" value={post.author} onChange={handleChange} /><br />
+                <br/>
+
+                <label for="description">Description</label><br />
+                <textarea rows="5" cols="50" id="description" value={post.description} onChange={handleChange} >
+                </textarea>
+                <br/>
+                <input type="submit" value="Submit" />
+                <button className="deleteButton">Delete</button>
+            </form>
+        </div>
+    )
+}
+
+export default EditPost
